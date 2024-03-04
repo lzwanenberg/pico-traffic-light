ead306d4b5769735e15d963d17350392fa904f74
Luc Zwanenberg <luc.zwanenberg@gmail.com>
2024-03-04 15:13:19 +0100

    Implement test hook

--- run/python/test-watcher.py
+++ run/python/test-watcher.py
@@ -2,19 +2,29 @@
 
 import subprocess
 import inotify.adapters
-import os
-import time
 from threading import Timer
 
-debounce_time = 0.05  # 50 milliseconds
+debounce_time = 0.05
 timer = None
 
+
 build_command = ["./run/build.sh"]
-test_command = ["./run/test.sh", "-r", "compact", "-s"]
+
+def get_filename(filepath):
+    return filepath.split('/')[-1]
+
+def get_filename_stem(filepath):
+    return get_filename(filepath).split(".")[0]
+
+def get_tag(filepath):
+    return f'[#{get_filename_stem(filepath)}.test]'
+
+def get_test_command(filepath):
+    return ["./run/test.sh", "-r", "compact", "-s", "-#", get_tag(filepath)]
 
 def run_tests(filepath):
     subprocess.call(build_command)
-    subprocess.call(test_command)
+    subprocess.call(get_test_command(filepath))
 
 def debounce_run_script(filepath):
     global timer
