c35fda304abd454aee301b041840d65821183f74
Luc Zwanenberg <luc.zwanenberg@gmail.com>
2024-03-05 16:44:15 +0100

    Implement writing to GPIO pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include "factorial/factorial.hpp"
-#include "pico_w/pico_w.hpp"
+// #include "pico_w/pico_w.hpp"
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/gpio.h"
+
+#define RED_LED_PIN 0
+#define GREEN_LED_PIN 1
 
 int main() {
-    int factorial_result1 = Factorial(5);
-    // int factorial_result2 = Factorial(6);
-    // int factorial_result3 = Factorial(7);
-    
+    int sleep_time_ms = Factorial(6);
 
-    if (!PicoW::initialize()) {
-        printf("Initialization failed");
-    }
+    gpio_init(RED_LED_PIN);
+    gpio_set_dir(RED_LED_PIN, GPIO_OUT);
+    gpio_pull_up(RED_LED_PIN);
 
-    while (true) {
-        PicoW::OnboardLED::turn_on();
-        PicoW::sleep_ms(factorial_result1);
-        PicoW::OnboardLED::turn_off();
-        PicoW::sleep_ms(factorial_result1);
+    gpio_init(GREEN_LED_PIN);
+    gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
+    gpio_pull_up(GREEN_LED_PIN);
+
+    while (true)
+    {
+        gpio_put(RED_LED_PIN, 1);
+        gpio_put(GREEN_LED_PIN, 0);
+        sleep_ms(sleep_time_ms);
+        gpio_put(RED_LED_PIN, 0);
+        gpio_put(GREEN_LED_PIN, 1);
+        sleep_ms(sleep_time_ms);
     }
 }
